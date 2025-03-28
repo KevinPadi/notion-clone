802f4c27ea3411f8c26579119bebbb0f8c4f9108
Kevin <kevpadilla01@gmail.com>
2025-03-27 22:00:47 -0300

    feat: dashboard home page updated

---
+++ src/components/card-page.tsx
@@ -0,0 +1,34 @@
+import { FileText } from "lucide-react"
+import { Page } from "@/context/pages_context"
+
+type CardPagePropsType = {
+  item: Page
+}
+
+const CardPage = ({item}: CardPagePropsType) => {
+  return (
+    <li key={item._id} className="w-full max-w-80 group hover:cursor-pointer">
+      <article className="w-full border border-transparent group-hover:border-muted-foreground/70 h-60 transition-all ease-in-out bg-neutral-200 dark:bg-neutral-900 rounded-3xl overflow-hidden">
+        <div className="w-full h-36 relative p-1.5">
+          {
+            item.cover !== 'none' ? (
+              <img src={item.cover} alt="Cover image" className="size-full object-cover object-center rounded-2xl" />
+            ) : (
+              <div className="bg-muted size-full p-10 rounded-2xl" ></div>
+            )
+          }
+          <span className="absolute left-8 -bottom-3">
+            {
+              item.icon !== 'none' ? item.icon : <FileText className="text-muted-foreground stroke-[1.5] size-10"/>
+            }
+          </span>
+        </div>
+        <h2 className="text-lg mt-8 ms-9">
+          {item.name}
+        </h2>
+      </article>
+    </li>
+  )
+}
+
+export default CardPage
--- src/pages/dashboardHome.tsx
+++ src/pages/dashboardHome.tsx
@@ -1,8 +1,41 @@
+import CardPage from "@/components/card-page"
+import { useAuth } from "@/context/auth_context"
+import { usePagesContext } from "@/context/pages_context"
+import { FileText } from "lucide-react"
+import { useEffect, useState } from "react"
+
 const DashboardHome = () => {
+  const [greetingMessage, setGreetingMessage] = useState('')
+  const { user } = useAuth()
+  const { pages } = usePagesContext()
+
+  useEffect(() => {
+    const hours = new Date().getHours()
+
+    if (hours >= 6 && hours < 12) {
+      setGreetingMessage('Buenos días')
+    } else if (hours >= 12 && hours < 18) {
+      setGreetingMessage('Buenas tardes')
+    } else {
+      setGreetingMessage('Buenas noches')
+    }
+  
+  }, [])
+  
   return (
-    <div className="p-4 bg-white rounded-xl shadow">
-      <h1 className="text-xl font-bold">Dashboard Home</h1>
-      <p>Bienvenido al dashboard.</p>
+    <div className="p-1 max-w-2xl w-full self-center mt-6 space-y-20">
+      <h1 className="text-4xl font-medium text-center">
+        {greetingMessage}, {user?.name}
+      </h1>
+      <section>
+        <ul className="flex flex-col sm:flex-row items-center gap-4 justify-between flex-wrap">
+          {
+            pages?.map((item) => (
+              <CardPage item={item} />
+            ))
+          }
+        </ul>
+      </section>
     </div>
   )
 }
