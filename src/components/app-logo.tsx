8cfa1c1c9379a26d30e8255acb78e8bb2f9aa88a
Kevin <kevpadilla01@gmail.com>
2025-04-21 21:39:21 -0300

    feat: add Notiony logo to AppLogo component

--- src/components/app-logo.tsx
+++ src/components/app-logo.tsx
@@ -2,15 +2,14 @@ import {
   SidebarMenu,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
-import NotionIcon from "./icons/notion-icon"
-
+import NotionLogo from "/public/notiony-logo.png"
 export function AppLogo() {
 
   return (
     <SidebarMenu>
       <SidebarMenuItem className="font-bold flex items-center gap-3">
-        <div className="size-8 bg-emerald-600 p-1 rounded-lg flex items-center justify-center">
-          <NotionIcon />
+        <div className="size-8 p-1 rounded-lg flex items-center justify-center">
+          <img src={NotionLogo} alt=" logo" />
         </div>
         Notiony 
       </SidebarMenuItem>
